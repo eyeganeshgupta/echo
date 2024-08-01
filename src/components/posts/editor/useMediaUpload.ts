b362734a75d81041413aa1a007b6d902070edbe8
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-08-01 10:04:26 +0530

    📝post-media-uploads-image-and-video🖼️🎞️

---
+++ src/components/posts/editor/useMediaUpload.ts
@@ -0,0 +1,110 @@
+import { useToast } from "@/components/ui/use-toast";
+import { useUploadThing } from "@/lib/uploadthing";
+import { useState } from "react";
+
+export interface Attachment {
+  file: File;
+  mediaId?: string;
+  isUploading: boolean;
+}
+
+export default function useMediaUpload() {
+  const { toast } = useToast();
+
+  const [attachments, setAttachments] = useState<Attachment[]>([]);
+
+  const [uploadProgress, setUploadProgress] = useState<number>();
+
+  const { startUpload, isUploading } = useUploadThing("attachment", {
+    onBeforeUploadBegin(files) {
+      const renamedFiles = files.map((file) => {
+        const extension = file.name.split(".").pop();
+        return new File(
+          [file],
+          `attachment_${crypto.randomUUID()}.${extension}`,
+          {
+            type: file.type,
+          },
+        );
+      });
+
+      setAttachments((prev) => [
+        ...prev,
+        ...renamedFiles.map((file) => ({ file, isUploading: true })),
+      ]);
+
+      return renamedFiles;
+    },
+    onUploadProgress: setUploadProgress,
+    onClientUploadComplete(res) {
+      setAttachments((prev) =>
+        prev.map((attachment) => {
+          const uploadResult = res.find((result) => {
+            return result.name === attachment.file.name;
+          });
+
+          if (!uploadResult) {
+            return attachment;
+          }
+
+          return {
+            ...attachment,
+            mediaId: uploadResult.serverData.mediaId,
+            isUploading: false,
+          };
+        }),
+      );
+    },
+    onUploadError(e) {
+      setAttachments((prev) =>
+        prev.filter((attachment) => !attachment.isUploading),
+      );
+      toast({
+        variant: "destructive",
+        description: e.message,
+      });
+    },
+  });
+
+  function handleStartUpload(files: File[]) {
+    if (isUploading) {
+      toast({
+        variant: "destructive",
+        description: "Please wait for the current upload to finish.",
+      });
+      return;
+    }
+
+    if (attachments.length + files.length > 5) {
+      toast({
+        variant: "destructive",
+        description: "You can only upload up to 5 attachments per post.",
+      });
+      return;
+    }
+
+    startUpload(files);
+  }
+
+  function removeAttachment(fileName: string) {
+    setAttachments((prev) =>
+      prev.filter((attachment) => {
+        return attachment.file.name !== fileName;
+      }),
+    );
+  }
+
+  function reset() {
+    setAttachments([]);
+    setUploadProgress(undefined);
+  }
+
+  return {
+    startUpload: handleStartUpload,
+    attachments,
+    isUploading,
+    uploadProgress,
+    removeAttachment,
+    reset,
+  };
+}
