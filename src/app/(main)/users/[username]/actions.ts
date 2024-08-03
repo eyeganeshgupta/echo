6611ecda1845f0665192b2c014f83fc182945031
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-08-03 10:14:39 +0530

    feature: messages-using-stream💬

--- src/app/(main)/users/[username]/actions.ts
+++ src/app/(main)/users/[username]/actions.ts
@@ -2,6 +2,7 @@
 
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
+import streamServerClient from "@/lib/stream";
 import { getUserDataSelect } from "@/lib/types";
 import {
   updateUserProfileSchema,
@@ -17,11 +18,22 @@ export async function updateUserProfile(values: UpdateUserProfileValues) {
     throw new Error("Unauthorized");
   }
 
-  const updateUser = await prisma.user.update({
-    where: { id: user.id },
-    data: validatedValues,
-    select: getUserDataSelect(user.id),
+  const updatedUser = await prisma.$transaction(async (tx) => {
+    const updatedUser = await tx.user.update({
+      where: { id: user.id },
+      data: validatedValues,
+      select: getUserDataSelect(user.id),
+    });
+
+    await streamServerClient.partialUpdateUser({
+      id: user.id,
+      set: {
+        name: validatedValues.displayName,
+      },
+    });
+
+    return updatedUser;
   });
 
-  return updateUser;
+  return updatedUser;
 }
